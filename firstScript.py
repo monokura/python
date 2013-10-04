11e5391599c3bb45e80f84c28d394343d7123789
shirakura <shirakurakento@gmail.com>
2013-10-04 16:52:04 +0900

    sample 01

---
+++ firstScript.py
@@ -0,0 +1,24 @@
+#coding: UTF-8
+
+msg = u"こんちわ"
+
+print msg
+print u"無駄!" * 10
+print len("aaaaaaaaaaa")
+print 'it\'s a pen.'
+
+print "================"
+s = "abcdefghi"
+print s[1:-1]
+print s.find("g")
+
+print "==============="
+
+print 2 ** 2 * 3
+print 10 // 3.0
+
+print "=============="
+
+print 5 + int("5")
+age = 20
+print "I am " + str(age) + " years old."
