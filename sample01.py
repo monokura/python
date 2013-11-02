025e4475dffd665f921bbcabba2423d51d5cb898
shirakura <shirakurakento@gmail.com>
2013-11-03 08:59:08 +0900

    win ノートPCとも同期

---
+++ sample01.py
@@ -0,0 +1,14 @@
+#! c:/python27/python.exe
+# coding: utf-8
+
+# 文字列の練習
+test_str = "this"
+test_str = test_str + " is"
+test_str = test_str + " a"
+test_str = test_str + " pen."
+
+print test_str
+
+test_int = 100
+
+print str(test_int) + u"円"
---
+++ test.py
@@ -0,0 +1,11 @@
+#! c:/Python27/python.exe
+# coding: UTF-8
+
+print u"モジュールのロード"
+
+def test():
+		print u"関数 : testを呼び出しました"
+
+if __name__ == "__main__":
+		print "python-izm"
+		test()
